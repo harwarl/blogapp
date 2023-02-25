661699282e652b0bce0ff6332847c93b720f5dd0
harwarl <harwarl87@gmail.com>
2023-02-24 18:14:38 -0800

    icons now resize

--- src/hooks/useWindowSize.js
+++ src/hooks/useWindowSize.js
@@ -1,23 +1,25 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 
 const useWindowSize = () => {
     const [windowSize, setWindowSize] = useState({
         width: undefined,
         height: undefined
-    })
+    });
 
     useEffect(() => {
+
         const handleResize = () => {
             setWindowSize({
                 width: window.innerWidth,
                 height: window.innerHeight
-            })
-
-            handleResize();
-            window.addEventListener("resize", handleResize);
-
-            return () => window.removeEventListener("resize", handleResize);
+            });
         }
+
+        handleResize();
+
+        window.addEventListener("resize", handleResize);
+
+        return () => window.removeEventListener("resize", handleResize);
     }, [])
 
     return windowSize;
